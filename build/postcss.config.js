49dc554e47849fcf16ccf069da7f5fc4ffcf2b43
胡英军 <yingjun.hu@geely.com>
2018-06-01 13:32:39 +0800

    feat: 加入dll、sweet-sdk

--- build/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// 针对webpack3 配置的postcss.config.js
-const utils = require("./utils.js");
-const postcssPlugins = utils.postcssPlugins();
-
-module.exports = {
-    plugins: postcssPlugins
-} 
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ global.env = minimist(process.argv.slice(2), { string: 'env', default: { env: pr
 global.envConfig = require('./config')[global.env];
 const gulp       = require('gulp');
 const requireDir = require('require-dir');
-requireDir('./build/tasks/', { recurse: true });
+
+requireDir('./node_modules/@sweetui/sweet-sdk/build/tasks/', { recurse: true });
 
 
 console.log('  ');
@@ -13,14 +14,3 @@ console.log('  \\__ \\| | /| / // _ \\ / _ \\ / __/  / / / / / /  ');
 console.log(' ___/ /| |/ |/ //  __//  __// /_   / /_/ /_/ /   ');
 console.log('/____/ |__/|__/ \\___/ \\___/ \\__/   \\____//___/   ');
 console.log('  ');
-
-
-/**
- * gulp默认启动配置
- */
-gulp.task('default', ['clean:all'], function() {
-	gulp.start('server');
-});
-
-
-gulp.task('build', ['js']);
